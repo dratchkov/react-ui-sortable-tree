7ed76b4606d30d8b28de8bee6a18ac49def73ae0
David Ratchkov <ratchkov@yahoo.com>
2022-03-18 15:34:31 -0700

    Fix getting width of element when scroll bar is shown

--- dist/tree.js
+++ dist/tree.js
@@ -118,7 +118,7 @@ proto.moveIndex = function (index, dragInfo, e, canMoveFn) {
 
     var treeRect = treeEl.getBoundingClientRect();
     var trgCoord = {
-      x: treeRect.right - 1 - (window.innerWidth - document.documentElement.clientWidth), // scroll bar width, from https://stackoverflow.com/questions/986937/how-can-i-get-the-browsers-scrollbar-sizes
+      x: treeEl.clientWidth - 1, // scroll bar width, from https://stackoverflow.com/questions/986937/how-can-i-get-the-browsers-scrollbar-sizes
       y: dragDirs.vrt >= 0 ? dragRect.bottom : dragRect.top
     };
     var hovMnodeEl = document.elementFromPoint(trgCoord.x, trgCoord.y);
